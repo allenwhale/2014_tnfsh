3d1ee01d53469b75f9e68a8a761d27c8bd40048c
allenwhale <allenwhale@yahoo.com.tw>
2014-12-09 12:02:50 +0800

    update G

--- G/ans.cpp
+++ G/ans.cpp
@@ -1,56 +1,73 @@
 #include <stdio.h>
-#include <string.h>
-#include <map>
 #include <algorithm>
+#include <string.h>
 #include <vector>
 using namespace std;
-vector<int>vc[200010];
-int dp[200010],f[200010];
-unsigned MOD,A,B;
-int Rand()
-{
-	return (int)(A=(((A<<4)|B)%MOD));
-}
-int M;
-map<int,int>mp;
-int sn=0;
-int find(int x)
-{
-	if(mp.find(x)==mp.end())mp[x]=sn++;
-	return mp[x];
-}
-int dfs(int x)
-{
-	if(dp[x]!=-1)return dp[x];
-	dp[x]=1;
-	for(int &v:vc[x])
-	{
-		dp[x]=max(dp[x],1+dfs(v));
-	}
-	return dp[x];
-}
-int findx(int x)
-{
-	return x==f[x]?x:f[x]=find(f[x]);
-}
+#define MAXN 100
+int N,M,S;
+int dis[MAXN+10][MAXN+10];
+int dis2[MAXN+10][MAXN+10];
 int main()
 {
-	scanf("%u %u %u %d",&A,&B,&MOD,&M);
-	for(int i=0;i<=min((int)MOD,M*2);i++)
-		f[i]=i;
+	memset(dis,0x3f,sizeof(dis));		
+	scanf("%d %d %d",&N,&M,&S);
 	for(int i=0;i<M;i++)
 	{
-		int a,b;
-		a=Rand();b=Rand();
-		int ta,tb;
-		ta=find(a),tb=find(b);
-		if(findx(ta)==findx(tb))continue;
-		vc[ta].push_back(tb);
+		int a,b,c;
+		scanf("%d %d %d",&a,&b,&c);
+		dis[a][b]=dis[b][a]=min(dis[a][b],c);
 	}
-	memset(dp,-1,sizeof(dp));
-	int ans=0;
-	for(int i=0;i<sn;i++)
-		ans=max(ans,dfs(i));
-	printf("%d\n",ans);
+	for(int i=0;i<N;i++)
+		dis[i][i]=0;
+	for(int k=0;k<N;k++)
+	{
+		for(int i=0;i<N;i++)
+		{
+			for(int j=0;j<N;j++)
+			{
+				if(dis[i][j]>dis[i][k]+dis[k][j])
+				{
+					dis[i][j]=dis[i][k]+dis[k][j];
+				}
+			}
+		}
+	}
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			if(dis[i][j]<=S)
+			{
+				dis2[i][j]=1;
+			}
+			else dis2[i][j]=0x3f3f3f3f;
+		}
+	}
+	for(int i=0;i<N;i++)
+		dis2[i][i]=0;
+	for(int k=0;k<N;k++)
+	{
+		for(int i=0;i<N;i++)
+		{
+			for(int j=0;j<N;j++)
+			{
+				if(dis2[i][j]>dis2[i][k]+dis2[k][j])
+				{
+					dis2[i][j]=dis2[i][k]+dis2[k][j];
+				}
+
+			}
+		}
+	}
+	int Q;
+	scanf("%d",&Q);
+	while(Q--)
+	{
+		int a,b;
+		scanf("%d %d",&a,&b);
+		if(dis2[a][b]==0x3f3f3f3f)
+			puts("Impossible");
+		else printf("%d\n",dis2[a][b]);
+	}
 	return 0;
 }
