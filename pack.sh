be2f560dea6de5a8fd751c0c30847e7f8ab7e527
allenwhale <allenwhale@yahoo.com.tw>
2014-12-12 21:08:38 +0800

    dist

---
+++ pack.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+all="A B C D E F G"
+IFS=" "
+rm -rf "dist" > /dev/null
+mkdir "dist"
+for i in $all ;do
+	rm -f  "$i/$i.tar.xz"
+	rm -rf "$i/http" > /dev/null
+	mkdir "$i/http"
+	cp "$i/cont.html" "$i/http"
+	tar Jcvf "dist/$i.tar.xz" "$i/res" "$i/http"
+done
